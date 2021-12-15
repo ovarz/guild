4bed8f5a71ffa1bc6e0274a2590b48bcbbb06cc5
ovarz <ovarz_03@yahoo.com>
2021-12-15 12:07:35 +0700

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -2,7 +2,7 @@
     <div class="column-small-container">
     
       <section class="base-container live-chat">
-        <script id="cid0020000300535527899" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"lovebeatfc","arch":"js","styles":{"a":"212121","b":100,"c":"ffffff","d":"ffffff","e":"212121","g":"e0e0e0","h":"212121","j":"ffffff","l":"424242","m":"FFFFFF","p":"10","q":"212121","r":100,"ab":false,"sbc":"212121","surl":0,"allowpm":0}}</script>
+        <iframe style="width:100%; height:480px; margin-top:0px;" width="300" height="480" src="https://www.youtube.com/live_chat?v=-5KAN9_CzSA&amp;embed_domain=www.ov4rz.com&dark_theme=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
       </section>
 
     </div>
