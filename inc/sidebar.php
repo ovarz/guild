e8e83859843987a9fad13fefdeefc040409d38d5
ovarz <ovarz_03@yahoo.com>
2021-12-15 12:08:49 +0700

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -2,7 +2,7 @@
     <div class="column-small-container">
     
       <section class="base-container live-chat">
-        <iframe style="width:100%; height:480px; margin-top:0px;" width="300" height="480" src="https://www.youtube.com/live_chat?v=-5KAN9_CzSA&amp;embed_domain=www.ov4rz.com&dark_theme=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+        <iframe style="width:100%; height:480px; margin-top:0px;" width="300" height="480" src="https://www.youtube.com/live_chat?v=FaXe7LL5VWU&amp;embed_domain=www.ov4rz.com&dark_theme=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
       </section>
 
     </div>
