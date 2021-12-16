e665ce8e37696bd1e09447efe10b82fd18aaef33
ovarz <ovarz_03@yahoo.com>
2021-12-17 02:13:00 +0700

    a

--- inc/menu-bottom.php
+++ inc/menu-bottom.php
@@ -1,6 +1,6 @@
-<menu class="menu-bottom mobile-only <?php if($kanal == 'home') { ?>bg-transparent<?php } ?>">
+<nav class="menu-bottom mobile-only <?php if($kanal == 'home') { ?>bg-transparent<?php } ?>">
   <?php $menu_bottom='yes'; require ($_SERVER['GUILD'].'inc/menu-main.php')?>
-</menu>
+</nav>
 
 <menu id="popup-Menu" class="popup-box popup-menu mobile-only">
   <?php $menu_bottom='no'; require ($_SERVER['GUILD'].'inc/menu-main.php')?>
