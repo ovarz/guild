32c11812148547bb1f614f105a32fd943ba48b27
ovarz <ovarz_03@yahoo.com>
2022-01-07 23:46:11 +0700

    a

--- module/member-list.php
+++ module/member-list.php
@@ -1,4 +1,4 @@
-<a aria-label="Member" title="Member" class="member-box" href="member/detail.php">
+<a aria-label="Member" title="Member" class="member-box" href="officer/detail.php">
   <div class="member-thumb content_center">
     <div class="member-thumb-circle flex_ori thumb-loading">
       <img alt="img_title" class="lazyload" data-original="img/sample/member-<?php echo rand(1,20); ?>.jpg">
--- officer/detail.php
+++ officer/detail.php
@@ -14,17 +14,55 @@
       <div class="column-big">
         <div class="column-big-container">
         
-          <section class="base-container detail-profile">
-            <div class="detail-profile-thumb dpt-<?php echo $channel; ?> content_center">
-              <div class="dpt-circle">
-                <div class="dpt-inside flex_ori thumb-loading">
-                  <img alt="img_title" class="lazyload" data-original="img/sample/<?php echo $channel; ?>-1.jpg">
-                </div>
+          <section class="base-container detail-character">
+            <div class="detail-character-thumb">
+              <div class="dct-frame flex_ori thumb-loading">
+                <img alt="img_title" class="lazyload" data-original="https://img2.finalfantasyxiv.com/f/81f3575db9d645095353c5a958e0936a_7b33d33ae3ecb996f778a5f67a6a0af6fl0_640x873.jpg?1641568260&_ga=2.88219163.1279051496.1641567462-1488581814.1639649965">
               </div>
             </div>
-            <div class="detail-profile-info">
-              <h1 class="dpi-name">Tifanna Lockheart</h1>
-              <h2 class="dpi-date">September 25th</h2>
+            <div class="detail-character-info">
+              <h2 class="dci-title">The Heart's Beat</h2>
+              <h1 class="dci-name">Tifanna Lockheart</h1>
+              <h3 class="dci-world">Diabolos (Crystal)</h3>
+              <div class="dci-date">00 January 0000 - Level 90</div>
+              <div class="dci-level">
+                <div class="dci-level-icon flex_ori content_center">
+                  <img alt="img_title" class="lazyload" data-original="https://img.finalfantasyxiv.com/lds/h/7/WdFey0jyHn9Nnt1Qnm-J3yTg5s.png">
+                </div>
+                <div class="dci-level-label flex_ori content_center">
+                  <img alt="img_title" class="lazyload" data-original="https://img.finalfantasyxiv.com/lds/h/N/r_T2Y5aKI0A8RytpzhdBBLtRdE.png">
+                </div>
+              </div>
+              <div class="dci-other">
+               <ul class="dci-more">
+                  <li class="dci-more-label">Race / Clan</li>
+                  <li class="dci-more-data">Viera / Veena</li>
+                </ul>
+                <ul class="dci-more">
+                  <li class="dci-more-label">Guardian</li>
+                  <li class="dci-more-data">Menphina, the Lover</li>
+                </ul>
+                <ul class="dci-more">
+                  <li class="dci-more-label">Grand Company</li>
+                  <li class="dci-more-data">Order of the Twin Adder / Second Serpent Lieutenant</li>
+                </ul>
+                <ul class="dci-more">
+                  <li class="dci-more-label">Free Company</li>
+                  <li class="dci-more-data">LoveBeat</li>
+                </ul>
+                <ul class="dci-more">
+                  <li class="dci-more-label">Minions</li>
+                  <li class="dci-more-data">00/0000 [00%]</li>
+                </ul>
+                <ul class="dci-more">
+                  <li class="dci-more-label">Mounts</li>
+                  <li class="dci-more-data">00/0000 [00%]</li>
+                </ul>
+                <ul class="dci-more">
+                  <li class="dci-more-label">Achievement</li>
+                  <li class="dci-more-data">00/0000 [00%]</li>
+                </ul>
+              </div>
             </div>
           </section>
           
